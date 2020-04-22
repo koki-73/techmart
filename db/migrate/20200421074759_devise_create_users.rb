e038b96aebf7f9f4e78db73da420c8d4ec78904f
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-22 22:41:59 +0900

    マイグレーション前コミットuser2

--- db/migrate/20200421074759_devise_create_users.rb
+++ db/migrate/20200421074759_devise_create_users.rb
@@ -7,14 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :password,           null: false
       t.string :first_name,         null: false
       t.string :family_name,        null: false
       t.string :first_name_kana,    null: false
       t.string :family_name_kana,   null: false
-      t.string :first_name,         null: false
-      t.date :birth_year,           null: false
-      t.date :birth_month,          null: false
       t.date :birth_day,            null: false
       ## Recoverable
       t.string   :reset_password_token
