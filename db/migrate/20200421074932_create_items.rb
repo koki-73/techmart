a2d86a42a73b775582ff00aabf1d7504ef18537b
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-23 11:38:00 +0900

    itemテーブルDB設計

--- db/migrate/20200421074932_create_items.rb
+++ db/migrate/20200421074932_create_items.rb
@@ -1,8 +1,23 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name,                       null: false
+      t.integer :price,                     null: false
+      t.enum :status,                       null: false
+      t.references :brand,                  foreign_key: true
+      t.references :buyer,                  foreign_key: true
+      t.references :shopping_cost,          null: false,  foreign_key: true
+      t.references :category,               null: false, foreign_key: true
+      t.references :user,                   null: false, foreign_key: true
+      t.references :item_image,             null: false, foreign_key: true
+      t.references :size,                   null: false, foreign_key: true
+      t.references :delivery_method,        null: false, foreign_key: true
+      t.references :delivery_existence,     null: false, foreign_key: true
+      t.references :item_explanation,       null: false, foreign_key: true
+      t.references :preparation_day,        null: false, foreign_key: true
       t.timestamps
     end
   end
 end
+
+
