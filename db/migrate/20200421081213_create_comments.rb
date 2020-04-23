fb640d1bb1e382f59562b49c7fac572816744dea
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-23 18:08:37 +0900

    テーブル設計コミット

--- db/migrate/20200421081213_create_comments.rb
+++ db/migrate/20200421081213_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
-      t.timestamps
+      t.text :comement
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.timestamps 
     end
   end
 end
--- db/migrate/20200421081422_create_user_rates.rb
+++ db/migrate/20200421081422_create_user_rates.rb
@@ -2,6 +2,9 @@ class CreateUserRates < ActiveRecord::Migration[5.2]
   def change
     create_table :user_rates do |t|
 
+      t.references :user, null:false, foreign_key:true
+      t.references :item, null:false, foreign_key:true
+      t.string :image
       t.timestamps
     end
   end
