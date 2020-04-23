8060708b77ac99809b681aea6b5efbb1c2eb8e71
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-23 12:43:45 +0900

    itemテーブルDB設計3

--- db/migrate/20200421074932_create_items.rb
+++ db/migrate/20200421074932_create_items.rb
@@ -5,14 +5,14 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :price,                     null: false
       t.enum :status,                       null: false
       t.references :brand,                  foreign_key: true
-      t.references :buyer_id,                  foreign_key: true
-      t.references :shopping_cost,          null: false,  foreign_key: true
+      t.references :buyer_id,               foreign_key: true
+      t.references :delivery_fee,           null: false,  foreign_key: true
       t.references :category,               null: false, foreign_key: true
       t.references :user,                   null: false, foreign_key: true
       t.references :item_image,             null: false, foreign_key: true
       t.references :size,                   null: false, foreign_key: true
       t.references :delivery_method,        null: false, foreign_key: true
-      t.references :delivery_existence,     null: false, foreign_key: true
+      t.references :ship_location,          null: false, foreign_key: true
       t.references :item_explanation,       null: false, foreign_key: true
       t.references :preparation_day,        null: false, foreign_key: true
       t.timestamps
