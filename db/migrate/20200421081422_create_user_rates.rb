dc6fbca44fa7783cf45c9681c02d5ca64066f5af
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-24 14:47:55 +0900

    テーブル変更コミット6

--- db/migrate/20200421081422_create_user_rates.rb
+++ db/migrate/20200421081422_create_user_rates.rb
@@ -1,9 +1,10 @@
 class CreateUserRates < ActiveRecord::Migration[5.2]
   def change
     create_table :user_rates do |t|
+
       t.references :user, null:false, foreign_key: true
       t.references :item, null:false, foreign_key: true
-      t.text :rate, null:false, foreign_key: true
+      t.string :rate, null:false, foreign_key: true
       t.timestamps
     end
   end
