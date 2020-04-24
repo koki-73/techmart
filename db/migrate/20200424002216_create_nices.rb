6ef0da613e34e67269ce4ea3727324b9e20686d0
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-24 10:12:26 +0900

    テーブル設計コミット4

--- db/migrate/20200424002216_create_nices.rb
+++
@@ -1,8 +0,0 @@
-class CreateNices < ActiveRecord::Migration[5.2]
-  def change
-    create_table :nices do |t|
-      t.references :item,                   null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
