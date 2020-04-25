a89b631db894635aefc1b8c8e1680c16d1c301aa
keizoukatagiri <kotetu51021996@gmail.com>
2020-04-25 11:24:35 +0900

    クレジットテーブル設計2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_04_23_090702) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -18,8 +17,6 @@ ActiveRecord::Schema.define(version: 2020_04_23_090702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -67,7 +64,6 @@ ActiveRecord::Schema.define(version: 2020_04_23_090702) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_code", null: false
     t.integer "tel_number"
@@ -81,8 +77,6 @@ ActiveRecord::Schema.define(version: 2020_04_23_090702) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
-
-
 
   create_table "user_rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -121,6 +115,4 @@ ActiveRecord::Schema.define(version: 2020_04_23_090702) do
   add_foreign_key "profiles", "users"
   add_foreign_key "user_rates", "items"
   add_foreign_key "user_rates", "users"
-
-
 end
